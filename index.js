602dbe6ea8abd6a08d8ec5eb24ebdf2d6ed76a12
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-04-02 22:14:36 +0700

    test

--- index.js
+++ index.js
@@ -165,11 +165,11 @@ function handleFileDrop(event) {
 
 
 const fileTypes = {
-    'image/png': 'img/image.svg',
-    'image/jpeg': 'img/image.svg',
-    'image/svg': 'img/image.svg',
-    'application/pdf': 'img/PDF.svg',
-    'application/folder': 'img/folder.svg',
+    'image/png': './img/image.svg',
+    'image/jpeg': './img/image.svg',
+    'image/svg': './img/image.svg',
+    'application/pdf': './img/PDF.svg',
+    'application/folder': './img/folder.svg',
     'application/doc': 'img/document.svg',
     'text/plain': 'img/document.svg',
 };
