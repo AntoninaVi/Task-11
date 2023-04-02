f8d55bcc3db3df10c6d6c873c0e0078ab1adf625
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-04-02 22:36:11 +0700

    test

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ const fileTypes = {
     'image/png': '../img/image.svg',
     'image/jpeg': './img/image.svg',
     'image/svg': './img/image.svg',
-    'application/pdf': '../img/PDF.svg',
+    'application/pdf': '/Task-11/img/PDF.svg',
     'application/folder': './img/folder.svg',
     'application/doc': 'img/document.svg',
     'text/plain': 'img/document.svg',
