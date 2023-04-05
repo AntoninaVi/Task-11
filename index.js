cd7d37d505951902c9738199e3cf585994051495
Antonina Vitkalova <tonatonotonya@gmail.com>
2023-04-05 23:36:30 +0700

    test

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ const fileTypes = {
     'image/jpeg': '/image.svg',
     'image/svg': '/image.svg',
     'image/png': '/image.svg',
-    'pdf': 'pdf.svg',
+    'application/pdf': '/pdf.svg',
     'application/folder': '/folder.svg',
     'application/doc': '/document.svg',
     'text/plain': '/document.svg',
